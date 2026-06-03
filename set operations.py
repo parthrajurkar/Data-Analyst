b15cba9bb019cc75314138a1fa3e60adf30edacd
Parth Rajurkar <pvrajurkar1@gmail.com>
2026-06-03 21:31:21 +0530

    Set basic operations

---
+++ set operations.py
@@ -0,0 +1,14 @@
+# Set operations like min,max,sorted,sum,len
+
+s1={1,2,3,4,5,6}
+print(s1)
+
+s2={10,20,30,40,50,60}
+print(s2)
+
+print(len(s1))
+print(min(s2))
+print(max(s2))
+print(sorted(s1))#result is stored in a list
+print(sorted(s2))#result is stored in a list
+print(sum(s1))
