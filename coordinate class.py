3cffc4f23cb8cabcb8380db9788d3396105ee256
Parth Rajurkar <pvrajurkar1@gmail.com>
2026-06-06 14:48:37 +0530

    Line Class-Object Created in Coordinate Class

--- coordinate class.py
+++ coordinate class.py
@@ -11,10 +11,25 @@ class Point:
     
     def distance_from_origin(self):
         return self.euclidean_distance(Point(0,0))
+    
+class Line:
+    def __init__(self,A,B,C):
+        self.A=A
+        self.B=B
+        self.C=C
         
+    def __str__(self):
+        return '{}x + {}y + {} = 0'.format(self.A,self.B,self.C)
+
+#Point Class Object created        
 p1=Point(0,0)
 p2=Point(-2,-1)
 
 print(p1.euclidean_distance(p2))
 
 print(p2.distance_from_origin())
+
+#Line class object created
+
+L1=Line(1,2,3)
+print(L1)
