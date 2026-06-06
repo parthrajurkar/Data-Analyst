b5d9e9e4724cf3c4f5ae3754b84ed547404fbc5e
Parth Rajurkar <pvrajurkar1@gmail.com>
2026-06-06 14:16:51 +0530

    distance from origin method added to coordinate class

--- coordinate class.py
+++ coordinate class.py
@@ -8,8 +8,13 @@ class Point:
     
     def euclidean_distance(self,other):
         return ((self.x_cod - other.x_cod)**2 + (self.y_cod - other.y_cod)**2)**0.5
+    
+    def distance_from_origin(self):
+        return self.euclidean_distance(Point(0,0))
         
 p1=Point(0,0)
 p2=Point(-2,-1)
 
 print(p1.euclidean_distance(p2))
+
+print(p2.distance_from_origin())
