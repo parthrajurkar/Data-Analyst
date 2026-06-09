9cd95791ef8d4816b2562b83cdcabedef9310031
Parth Rajurkar <pvrajurkar1@gmail.com>
2026-06-09 12:54:26 +0530

    comments added to Phone Inheritance.py

--- Phone inheritance.py
+++ Phone inheritance.py
@@ -1,3 +1,4 @@
+#Parent Class
 class Phone:
     def __init__(self,price,brand,camera):
         print("Inside Phone Constructor")
@@ -7,8 +8,11 @@ class Phone:
         
     def buy(self):
         print("Buying Phone")
-        
+
+#Child Class        
 class SmartPhone(Phone):
     pass
 
+#If child class doesn't have a constructor then constructor of parent class is called
 a=SmartPhone(40000,'Samsung',50)
+a.buy()
