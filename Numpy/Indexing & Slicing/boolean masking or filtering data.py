b6ed7601de45e94cf53840892829f9c8d3102417
Parth Rajurkar <pvrajurkar1@gmail.com>
2026-06-21 00:11:13 +0530

    boolean masking or filtering data used to print elements fulfilling certain condition

---
+++ Numpy/Indexing & Slicing/boolean masking or filtering data.py
@@ -0,0 +1,6 @@
+# boolean masking or filtering data is used as a condition to print elements
+# fulfilling certain condition
+
+import numpy as np
+arr=np.array([10,20,30,40,50,60])
+print(arr[arr > 25])
