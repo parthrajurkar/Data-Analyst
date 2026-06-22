afc183c004dba4f7b9f3b9245032773ade4420ae
Parth Rajurkar <pvrajurkar1@gmail.com>
2026-06-22 21:01:10 +0530

    splitting used to equally divide array into sub-arrays

---
+++ Numpy/Advanced Numpy/splitting.py
@@ -0,0 +1,11 @@
+"""Split is used to equally divide array into sub-arrays
+split,syntax - numpy.split(array,no. of splits)
+
+numpy.vsplit()
+numpy.hsplit()
+"""
+
+import numpy as np
+arr = np.array([10,20,30,40,50,60])
+print(np.split(arr,2))
+
