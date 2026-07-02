2ecd53bc22203b76c0833292010cf344b5612d2e
Parth Rajurkar <pvrajurkar1@gmail.com>
2026-07-02 10:07:42 +0530

    changes tob horizontal concat.py

--- Pandas/Merging & Joining/horizontal concat.py
+++ Pandas/Merging & Joining/horizontal concat.py
@@ -12,5 +12,5 @@ df_Region2=pd.DataFrame({
 
 # Horizontal Concat - row-wise
 #syntax - df=pd.concat([df1,df2],axis=0)
-df_concat=pd.concat([df_Region1,df_Region2],axis=1,ignore_index=True)
-print(df_concat)
+df_concat=pd.concat([df_Region1,df_Region2],axis=1)
+print(df_concat.to_string(index=False))
