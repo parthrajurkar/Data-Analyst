f57555d6777a1b6d51360e515723420b74c1b809
Parth Rajurkar <pvrajurkar1@gmail.com>
2026-07-23 23:59:52 +0530

    Created a db,SimpleStore added a Table Sales with values in it

---
+++ MySQL/simplestore.sql
@@ -0,0 +1,20 @@
+create database if not exists SimpleStore;
+use SimpleStore;
+
+create table Sales(
+Product varchar(50),
+Category varchar(50),
+Amount decimal(10,2),
+SaleDate date
+);
+
+insert into sales(Product,Category,Amount,SaleDate)
+values
+('Pen','Stationery',25.00,'2024-03-01'),
+('Notebook','Stationery',50.00,'2024-03-01'),
+('Mouse','Electronics',500.00,'2024-03-02'),
+('Keyboard','Electronics',700.00,'2024-03-02'),
+('Charger','Electronics',300.00,'2024-03-03'),
+('Bag','Accessories',1000.00,'2024-03-04');
+
+select * from sales;
