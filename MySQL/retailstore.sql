69226549b673826188644686f028c422e2e58106
Parth Rajurkar <pvrajurkar1@gmail.com>
2026-07-24 12:28:04 +0530

    created table orders

--- MySQL/retailstore.sql
+++ MySQL/retailstore.sql
@@ -5,3 +5,11 @@ create table Customers(
 CustomerID int primary key,
 CustomerName varchar(100)
 );
+
+create table orders(
+OrderID int primary key,
+Product varchar(50),
+Amount decimal(10,2),
+CustomerID int,
+foreign key(CustomerID) references Customer (CustomerID)
+);
